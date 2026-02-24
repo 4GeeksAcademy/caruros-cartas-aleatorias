b853c5c04dccf0eb271db98a27985173fe4eef21
caruros <ccangana1@gmail.com>
2026-02-24 00:28:53 +0000

    faltan detalles

--- src/app.js
+++ src/app.js
@@ -1,11 +1,21 @@
-import "bootstrap";
-import "./style.css";
+window.onload = function () {
+
+  const palos = ["♦", "♥", "♠", "♣"];
+  const palosNombre = ["diamond", "heart", "club", "spade"];
+  const valor = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  let paloRandom = Math.floor(Math.random() * 4);
+  let valorRandom = Math.floor(Math.random() * 13);
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  let card = document.getElementById("card");
+  let top = document.querySelector(".top");
+  let bottom = document.querySelector(".bottom");
+  let number = document.querySelector(".number");
+
+  top.innerHTML = palos[paloRandom];
+  bottom.innerHTML = palos[paloRandom];
+  number.innerHTML = valor[valorRandom];
+  card.className = "card";
+  card.classList.add(palosNombre[paloRandom]);
+}
